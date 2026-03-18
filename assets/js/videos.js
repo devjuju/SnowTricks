6458cf837bed444d0fead7945470e295d86438cd
devjuju <jleleubellpro1994@gmail.com>
2026-03-18 19:14:08 +0100

    file js update videos

--- assets/js/videos.js
+++ assets/js/videos.js
@@ -1,16 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-    /* -------- UTILS -------- */
-    const isDesktopLayout = (wrapper) =>
-        window.getComputedStyle(wrapper).flexDirection === "row";
-
-    const smartScroll = (wrapper, element) => {
-        element.scrollIntoView({
-            behavior: "smooth",
-            inline: isDesktopLayout(wrapper) ? "start" : "nearest",
-            block: isDesktopLayout(wrapper) ? "nearest" : "start",
-        });
-    };
-
     /* -------- VIDEO SECTION -------- */
     const videoWrapper = document.getElementById("video-wrapper");
 
